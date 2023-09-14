b6a2ca1366233b240faa711cf4c5725491a4dc7d
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-14 20:55:15 +0900

    💄 画像検索コンポーネントの作成

--- src/app/search/image/page.tsx
+++ src/app/search/image/page.tsx
@@ -1,7 +1,52 @@
+import Link from 'next/link'
 import React from 'react'
 
-const ImageSearchPage = () => {
-  return <div>ImageSearchPage</div>
+import ImageSearchResults from '@/components/ImageSearchResults'
+import WebSearchResults from '@/components/WebSearchResults'
+import type { ImageItem, SearchImageData } from '@/types/SearchData'
+
+const fetchSearchData = async (searchTerm: string) => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 300))
+    const res = await fetch(
+      `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.GOOGLE_SEARCH_CONTEXT_KEY}&q=${searchTerm}&searchType=image`,
+    )
+
+    if (!res.ok) {
+      console.log(res)
+      throw new Error('Something went wrong')
+    }
+
+    const data: SearchImageData = await res.json()
+
+    return data
+  } catch (err) {
+    throw new Error('Something went wrong')
+  }
+}
+
+type WebSearchPageProps = {
+  searchParams: { searchTerm: string }
+}
+
+const ImageSearchPage: React.FC<WebSearchPageProps> = async ({ searchParams }) => {
+  const data = await fetchSearchData(searchParams.searchTerm)
+  const results: ImageItem[] = data.items
+
+  if (!results)
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to hte homepage.
+          <Link href="/" className="text-blue-500 ml-2">
+            Home
+          </Link>
+        </p>
+      </div>
+    )
+
+  return <>{results && <ImageSearchResults results={data} />}</>
 }
 
 export default ImageSearchPage
--- src/types/SearchData.ts
+++ src/types/SearchData.ts
@@ -37,3 +37,40 @@ export type SearchData = {
   }
   items: Item[]
 }
+
+type Image = {
+  contextLink: string
+  height: number
+  width: number
+  byteSize: number
+  thumbnailLink: string
+  thumbnailHeight: number
+  thumbnailWidth: number
+}
+
+export type ImageItem = {
+  kind: string
+  title: string
+  htmlTitle: string
+  link: string
+  displayLink: string
+  snippet: string
+  htmlSnippet: string
+  mime: 'image/png'
+  fileFormat: 'image/png'
+  image: Image
+}
+
+export type SearchImageData = {
+  kind: string
+  url: { type: 'application/json'; template: string }
+  queries: { request: QueriesItem; nextPage: QueriesItem }
+  context: { title: string }
+  searchInformation: {
+    searchTime: number
+    formattedSearchTime: string
+    totalResults: string
+    formattedTotalResults: string
+  }
+  items: ImageItem[]
+}
